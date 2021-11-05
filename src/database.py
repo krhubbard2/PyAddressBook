67531c1219b8da06a4a402a0af225efc4b40d1d2
Kelby Hubbard <krhubbard2@alaska.edu>
2021-11-05 15:53:31 -0800

    Creates contacts table in database.

--- src/database.py
+++ src/database.py
@@ -6,7 +6,21 @@
 # Used to provide connection to database
 
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+# Create contacts table in database
+def createContactsTable():
+    createTableQuery = QSqlQuery()
+    return createTableQuery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            job VARCHAR(50),
+            email VARCHAR(40) NOT NULL
+        )
+        """
+    )
 
 # Create / open db connection
 def createConnection(dbName):
@@ -21,5 +35,6 @@ def createConnection(dbName):
             f"Database Error: {connection.lastError().text()}",
         )
         return False
-        
+    
+    createContactsTable()
     return True
